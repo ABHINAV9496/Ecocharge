40870c1d0555bb0ca2702d73d20f2a9f19e2d847
ABHINAV9496 <abhinavramesh74@gmail.com>
2026-06-18 15:15:31 +0530

    Register Trip and WalletTransaction models in admin with custom admin interfaces

--- django-backend/trips/admin.py
+++ django-backend/trips/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trip
 
-# Register your models here.
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('driver', 'origin', 'destination', 'distance_km', 'total_cost', 'created_at')
+    search_fields = ('driver__username', 'origin', 'destination')
+    date_hierarchy = 'created_at'
--- django-backend/wallet/admin.py
+++ django-backend/wallet/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WalletTransaction
 
-# Register your models here.
+@admin.register(WalletTransaction)
+class WalletTransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'transaction_type', 'amount', 'balance_after', 'created_at')
+    list_filter = ('transaction_type',)
+    search_fields = ('user__username',)
+    date_hierarchy = 'created_at'
