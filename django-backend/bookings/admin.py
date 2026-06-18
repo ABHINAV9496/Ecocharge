a3fc4cc3b4a500b24bb5ac252c5a5613addd807c
ABHINAV9496 <abhinavramesh74@gmail.com>
2026-06-18 15:14:34 +0530

    Register Booking model in admin with custom admin interface

--- django-backend/bookings/admin.py
+++ django-backend/bookings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'driver', 'slot', 'status', 'start_time', 'amount_charged')
+    list_filter = ('status',)
+    search_fields = ('driver__username',)
+    date_hierarchy = 'created_at'
