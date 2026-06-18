6fc64c4e911d980b5d5b61859cfe168442596c15
ABHINAV9496 <abhinavramesh74@gmail.com>
2026-06-18 15:15:17 +0530

    Register ChargingStation and ChargingSlot models in admin with custom admin interfaces

--- django-backend/stations/admin.py
+++ django-backend/stations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ChargingStation, ChargingSlot
 
-# Register your models here.
+@admin.register(ChargingStation)
+class ChargingStationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('name', 'address')
+
+@admin.register(ChargingSlot)
+class ChargingSlotAdmin(admin.ModelAdmin):
+    list_display = ('station', 'slot_type', 'status', 'rate_per_kwh')
+    list_filter = ('slot_type', 'status')
