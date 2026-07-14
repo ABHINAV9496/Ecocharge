3cdaecf3c18164f89609cc61afe8681715961460
ABHINAV9496 <abhinavramesh74@gmail.com>
2026-07-14 14:02:09 +0530

    refactor: remove unused useEffect hook from MapPage component

--- frontend/src/pages/MapPage.jsx
+++ frontend/src/pages/MapPage.jsx
@@ -1,16 +1,9 @@
-import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import MapView from '../components/map/MapView'
 
 export default function MapPage() {
   var location = useLocation()
   var routePlan = location.state?.routePlan || null
-
-  useEffect(function () {
-    if (location.state?.routePlan) {
-      window.history.replaceState({}, document.title)
-    }
-  }, [])
 
   return (
     <div className="h-screen w-screen overflow-hidden bg-gray-950">
